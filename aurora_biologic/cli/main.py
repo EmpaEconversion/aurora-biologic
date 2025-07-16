00cd08af65e34eb39afc73c8474a91dac617c25b
Graham Kimbell <88666181+g-kimbell@users.noreply.github.com>
2025-07-16 16:25:56 +0200

    Add pipelines command

--- aurora_biologic/cli/main.py
+++ aurora_biologic/cli/main.py
@@ -8,12 +8,32 @@ import typer
 
 from aurora_biologic import BiologicAPI
 
-app = typer.Typer()
+app = typer.Typer(add_completion=False)
 
 IndentOption = Annotated[int | None, typer.Option(help="Indent the output.")]
 PipelinesArgument = Annotated[list[str] | None, typer.Argument()]
 NumberOfPoints = Annotated[int, typer.Argument()]
 PathArgument = Annotated[Path, typer.Argument(help="Path to a file")]
+
+
+@app.command()
+def pipelines(
+    indent: IndentOption = None,
+) -> None:
+    """Return details of all connected instruments.
+
+    Returns a dictionary as a JSON string.
+
+    Example usage:
+    >>> biologic pipelines
+    {"MPG2-16-1": {"device_index": 0, "device_serial_number": 365 ... } ... }
+
+    Args:
+        indent (optional): an integer number that controls the identation of the printed output
+
+    """
+    with BiologicAPI() as bio:
+        typer.echo(json.dumps(bio.pipelines, indent=indent))
 
 
 @app.command()
