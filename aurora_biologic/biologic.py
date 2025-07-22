0bf2fde08126a2b2a3f469ee9a76512881f0cff6
Graham Kimbell <88666181+g-kimbell@users.noreply.github.com>
2025-07-22 13:38:47 +0200

    Check if files exist before starting

--- aurora_biologic/biologic.py
+++ aurora_biologic/biologic.py
@@ -277,7 +277,10 @@ class BiologicAPI:
         if output_path.is_dir():
             msg = "Must provide a full file path, not directory."
             raise ValueError(msg)
-        output_path.mkdir(parents=True, exist_ok=True)
+        if output_path.with_suffix(".mps").exists():
+            msg = "This experiment already exists! Use a new filepath."
+            raise ValueError(msg)
+        output_path.parent.mkdir(parents=True, exist_ok=True)
         dev_idx, channel_idx = self._get_pipeline_indices(pipeline)
         self._olecom_select_channel(dev_idx, channel_idx)
         self._olecom_run_channel(dev_idx, channel_idx, str(output_path))
