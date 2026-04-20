9f64db36002c96a122452e8934b24409b6f73edc
Graham Kimbell <88666181+g-kimbell@users.noreply.github.com>
2026-04-20 11:56:20 +0200

    Remove psutil dependency (#16)
    * Remove psutil dependency
    
    * Use tasklist to check if EC-lab running instead
    
    * Warn user if EC-lab was not open
    
    * Better compatibility for different EClab executable names

--- aurora_biologic/biologic.py
+++ aurora_biologic/biologic.py
@@ -16,7 +16,6 @@ from time import sleep
 from types import TracebackType
 from typing import Any
 
-import psutil
 from comtypes.client import CreateObject
 from platformdirs import user_config_dir
 
@@ -137,10 +136,32 @@ class BiologicAPI:
 
     def _open_eclab(self) -> None:
         """Open EC-lab if it is not already running."""
-        if not any("EClab" in proc.info["name"] for proc in psutil.process_iter(["name"])):
-            if eclab_path := self.CONFIG.get("eclab_path"):
+        eclab_name = Path(
+            self.CONFIG.get(
+                "eclab_path",
+                "C:/Program Files (x86)/EC-Lab/EClab.exe",
+            )
+        ).name
+        result = subprocess.run(
+            [r"C:\Windows\System32\tasklist.exe", "/FI", f"IMAGENAME eq {eclab_name}", "/NH"],
+            check=False,
+            capture_output=True,
+            text=True,
+        )
+        if eclab_name.lower() not in result.stdout.lower():
+            eclab_path = self.CONFIG.get("eclab_path")
+            if eclab_path:
+                if Path(eclab_path).suffix != ".exe":
+                    msg = f"eclab_path does not point to an executable: {eclab_path}"
+                    raise ValueError(msg)
                 if not os.getenv("AURORA_BIOLOGIC_MOCK_OLECOM"):
                     subprocess.Popen([eclab_path])
+                logger.critical(
+                    "EC-lab is not running. Starting %s. "
+                    "Devices may take a while to connect, "
+                    "try again in a minute if this command says all devices are offline.",
+                    eclab_path,
+                )
                 _sleep(2)  # To allow the program to initialize
             else:
                 msg = (
